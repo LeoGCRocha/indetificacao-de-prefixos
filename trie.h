ebce39ed5b337ce5c039b12f8e96cb128a7d71f9
unknown <gideonveloz@gmail.com>
2020-11-21 17:04:24 -0300

    fixed syntax errors

--- trie.h
+++ trie.h
@@ -19,108 +19,98 @@ class Trie {
     //! clear
     void clear();
     // insert
-    void insert(char *string, int position);
-    // track
-    int* track(char* string);
+    void insert(std::string data, int position);
+    // 
  private:
     class NoTrie {  // Elemento
      public:
         NoTrie() {
             position = 0;
             extension = 0;
-            letter = " ";
-            this->sons = new *NoTrie[26]
+            letter = ' '; 
             for (int i = 0; i < 26; i++) {
                 this->sons[i] = nullptr;
             }
         }
-
+        
         explicit NoTrie(char letter, unsigned long position, unsigned long extension) {
             this->letter = letter;
             this->position = position;
             this->extension = extension;
-            this->sons = new *NoTrie[26];
             for (int i = 0; i < 26; i++) {
                 this->sons[i] = nullptr;
             }
         }
 
-        void insert(std::string *word, unsigned long position, unsigned long extension);
-
-        char data() {  // getter: data
-            return data_;
-        }
-
-        const char data() const {  // getter const: data
-            return data_;
-        }
-
+        void insert(std::string word, unsigned long position, unsigned long end, NoTrie* head);
+        
         unsigned long getPosition() {
             return this->position;
         }
-
+        
         unsigned long getExtension() {
             return this->extension;
         }
-
-       NoTrie* sons getSons() {
-            return this->son;
-        }
-
+        
         char getLetter() {
             return this->letter;
         }
-
+        
         void setPosition(unsigned long p) {
             this->position = p;
         }
-
+        
         void setExtension(unsigned long e) {
             this->extension = e;
         }
-
+        
         void setLetter(char l) {
             this->letter = l;
         }
      private:
-        char letter;
-        NoTrie* sons[26];
+        char letter;        
+        NoTrie* sons[26];  
         unsigned long  position;
-        unsigned long  extension;
+        unsigned long  extension; 
     };
 
-    NoTrie* head;  // top
+    NoTrie* head;
 };
 
 }   // namespace structures
 #endif
 
+structures::Trie::~Trie() {
+    free(head);
+}
+
 structures::Trie::Trie() {
-    NoTrie *b = new NoTrie;
+    NoTrie *b = new NoTrie();
     head = b;
 }
 
-structures::Trie::NoTrie::insert(std::string *word, unsigned long position, unsigned long extension) {
+void structures::Trie::NoTrie::insert(std::string word, unsigned long position, unsigned long end, NoTrie* head) {
     NoTrie *b = head;
     for (int i = 0; i < word.size(); i++ ) {
         int ascii = int(word[i]) - 97;
         if (!b->sons[ascii]) {
-            b->sons[ascii] = new Node(word[i], 0, 0);
+            b->sons[ascii] = new NoTrie(word[i], 0, 0);
         } else {
             b = b->sons[ascii];
         }
-    }
+    } 
     b->setPosition(position);
-    b->setExtension(extension);
-}
+    b->setExtension(end);
+} 
 
-structures::Trie::insert(std::string *data, int position) {
+void structures::Trie::insert(std::string data, int position) {
     if (head == nullptr) {
-        throw std::out_of_range("invalid tree");
+        throw std::out_of_range("nullptr error.");
     } else {
         unsigned long posEnd = (unsigned long) data.find("]");
-        std::string *word = data.substr(1, posEnd - 1);
-        unsigned long end = ((unsigned long) data.size()) - (posEnd);
-        head->insert(word, position, data.size());
+        std::string word = data.substr(1, posEnd - 1);
+        unsigned long end = ((unsigned long) data.size()) - (posEnd); 
+        head->insert(word, position, end, head); 
     }
-}
+
+} 
