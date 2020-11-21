1cd20af3b4eef8b04c34f3206ea1e8f98b7b5a88
unknown <gideonveloz@gmail.com>
2020-11-21 17:43:51 -0300

    fix main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "trie.h"
 #include <stdio.h>
+#include <fstream>
+#include <iostream>
 
 int main() {
     
@@ -12,30 +14,16 @@ int main() {
     FILE *arq; /* File to read. */
 
     cin >> filename;  // Input
-    arq = fopen(filename.c_str(), "rt");
-    char *result;
-    char line[100]; // Existe uma maneira de pegar o tamanho?
-    if (arq == NULL)  {
-        cout << "Error" << endl;
-    } else {
-        int i = 1;
-        int position = 0;
-        while (!feof(arq)){
-              result = fgets(line, sizeof(line), arq);  // o 'fgets' lê até 99 caracteres ou até o '\n'
-        	  string textVal; // COMO PEGAR FRASE A FRASE DO ARQUIVO ?
-        	  for(int j = 0; j < sizeof(line); j++ ) {
-        	      cout << line[j];
-        	  }
-        	  trie.insert(textVal, position, sizeof(textVal))
-        	  position = position + sizeof(textVal) + 1;
-              i++;
-              cout << endl;
-        }
-        fclose(arq);
+    
+    ifstream file(filename.c_str());
+    if (file.is_open()){
+    	string line;
+    	int position = 0;
+    	while (getline(file, line)){
+        	tree.insert(line, position);
+    	    position = position + sizeof(line) + 1;
+    	}
     }
-    
-    // Devemos percorrer o arquivo .dic, para receber todas definições. 
-    
     while (1) {  // leitura das palavras ate' encontrar "0"
         cin >> word;
         if (word.compare("0") == 0) {
