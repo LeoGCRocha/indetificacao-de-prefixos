d165a7ac17ea597cf005a48f8587de4c4bc83a2a
Nicolas Vanz <nicavanz@hotmail.com>
2020-11-27 12:11:07 -0300

    fixed insertion function & added prefix counter function

--- main.cpp
+++ main.cpp
@@ -4,31 +4,33 @@
 #include <fstream>
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    
-    using namespace std;
     structures::Trie tree;
-    
+
     string filename;
     string word;
     FILE *arq; /* File to read. */
 
     cin >> filename;  // Input
-    
+
     ifstream file(filename.c_str());
     if (file.is_open()){
     	string line;
     	int position = 0;
     	while (getline(file, line)){
         	tree.insert(line, position);
-    	    position = position + sizeof(line) + 1;
+    	    position += sizeof(line) + 1;
     	}
     }
+    file.close();
     while (1) {  // leitura das palavras ate' encontrar "0"
         cin >> word;
         if (word.compare("0") == 0) {
             break;
-        } 
+        }
+        cout << tree.count_prefix(word) << endl;
     }
 
     return 0;
--- trie.h
+++ trie.h
@@ -20,19 +20,21 @@ class Trie {
     void clear();
     // insert
     void insert(std::string data, int position);
-    // 
+    //
+    int count_prefix(std::string word);
+    void display(void);
  private:
     class NoTrie {  // Elemento
      public:
         NoTrie() {
             position = 0;
             extension = 0;
-            letter = ' '; 
+            letter = '#';
             for (int i = 0; i < 26; i++) {
                 this->sons[i] = nullptr;
             }
         }
-        
+
         explicit NoTrie(char letter, unsigned long position, unsigned long extension) {
             this->letter = letter;
             this->position = position;
@@ -43,42 +45,47 @@ class Trie {
         }
 
         void insert(std::string word, unsigned long position, unsigned long end, NoTrie* head);
-        
+
+        int count_prefix(int &counter, NoTrie *node);
+
+        NoTrie *branch_point(std::string word, NoTrie *node);
+
+        void display(NoTrie* node, std::string word);
+
         unsigned long getPosition() {
             return this->position;
         }
-        
+
         unsigned long getExtension() {
             return this->extension;
         }
-        
+
         char getLetter() {
             return this->letter;
         }
-        
+
         void setPosition(unsigned long p) {
             this->position = p;
         }
-        
+
         void setExtension(unsigned long e) {
             this->extension = e;
         }
-        
+
         void setLetter(char l) {
             this->letter = l;
         }
      private:
-        char letter;        
-        NoTrie* sons[26];  
+        char letter;
+        NoTrie* sons[26];
         unsigned long  position;
-        unsigned long  extension; 
+        unsigned long  extension;
     };
 
     NoTrie* head;
 };
 
 }   // namespace structures
-#endif
 
 structures::Trie::~Trie() {
     free(head);
@@ -89,28 +96,83 @@ structures::Trie::Trie() {
     head = b;
 }
 
-void structures::Trie::NoTrie::insert(std::string word, unsigned long position, unsigned long end, NoTrie* head) {
-    NoTrie *b = head;
+void structures::Trie::NoTrie::insert(std::string word, unsigned long position, unsigned long end, NoTrie* node) {
+    NoTrie *b = node;
+    int ascii;
     for (int i = 0; i < word.size(); i++ ) {
-        int ascii = int(word[i]) - 97;
+        ascii = word[i] - 'a';
         if (!b->sons[ascii]) {
             b->sons[ascii] = new NoTrie(word[i], 0, 0);
-        } else {
-            b = b->sons[ascii];
         }
-    } 
+        b = b->sons[ascii];
+    }
     b->setPosition(position);
     b->setExtension(end);
-} 
+}
 
 void structures::Trie::insert(std::string data, int position) {
     if (head == nullptr) {
-        throw std::out_of_range("nullptr error.");
+        throw std::out_of_range("empty tree");
     } else {
         unsigned long posEnd = (unsigned long) data.find("]");
         std::string word = data.substr(1, posEnd - 1);
-        unsigned long end = ((unsigned long) data.size()) - (posEnd); 
-        head->insert(word, position, end, head); 
+        unsigned long end = ((unsigned long) data.size()) - (posEnd);
+        head->insert(word, position, end, head);
     }
+}
 
-} 
+int structures::Trie::count_prefix(std::string word) {
+    NoTrie *branch = head;
+    int counter = 0;
+    if (!head) throw std::out_of_range("empty tree");
+    branch = head->branch_point(word, head);
+    if (branch) return branch->count_prefix(counter, branch);
+    std::cout << "no branch point" << std::endl;
+    return 0;
+}
+
+structures::Trie::NoTrie *structures::Trie::NoTrie::branch_point(std::string word, NoTrie *node) {
+    NoTrie *branch = node;
+    for (int i = 0; i < word.size(); i++) {
+        if (branch->sons[word[i] - 'a']) branch = branch->sons[word[i] - 'a'];
+        else
+        return nullptr;
+    }
+    return branch;
+}
+
+int structures::Trie::NoTrie::count_prefix(int &counter, NoTrie *node) {
+    if (!node) throw std::invalid_argument("invalid node specified");
+    if (node->extension) counter++;
+    for (int i = 0; i < 26; i++) {
+        if (node->sons[i]) {
+            count_prefix(counter, node->sons[i]);
+        }
+    }
+    return counter;
+}
+
+
+void structures::Trie::display(void) {
+    if (!head) throw std::out_of_range("empty tree");
+    std::string word = "\0";
+    head->display(head, word);
+}
+
+void structures::Trie::NoTrie::display(NoTrie* node, std::string word) {
+    bool flag = false;
+    for (int i = 0; i < 26; i++) {
+        if (!flag) {
+            word.append(&node->letter);
+            flag = true;
+        }
+        if (node->sons[i]) {
+            display(node->sons[i], word);
+        }
+    }
+    if (node->extension) {
+        std::cout << word << std::endl;
+    }
+}
+
+#endif
